035b4733568110196d5e586dfdd80af0b6e85e8b
KanghoYoo <kangsick98@gmail.com>
2022-12-07 16:15:57 +0900

    Add: body 공통 css 적용

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Login from "./Login";
+import styled, { createGlobalStyle } from "styled-components";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Login />
     </div>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  box-sizing: border-box;
+  background: #f1f1f5;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+`;
 
 export default App;
