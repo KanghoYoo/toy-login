dd2d7e619f78beff0c1136b67d6763a9f906194b
KanghoYoo <kangsick98@gmail.com>
2022-12-08 15:58:12 +0900

    Add: react router dom 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import SignUp from "./SignUp";
 import { createGlobalStyle } from "styled-components";
+import Error404 from "./Error404";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/signup/" element={<SignUp />}></Route>
+          <Route path="/*" element={<Error404 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-const GlobalStyle = createGlobalStyle`
-body {
-  margin: 0;
-  padding: 0;
-  font-family: sans-serif;
-  box-sizing: border-box;
-  background: #f1f1f5;
-}
+// const GlobalStyle = createGlobalStyle`
+// body {
+//   margin: 0;
+//   padding: 0;
+//   font-family: sans-serif;
+//   box-sizing: border-box;
+//   background: #f1f1f5;
+// }
 
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-`;
+// *, *:before, *:after {
+//   box-sizing: inherit;
+// }
+// `;
 
 export default App;
